43995de9c192ea85af6d6c137a2a2c8b2c8f8003
Claurubio7 <claulazorubio27@gmail.com>
2025-09-24 14:20:36 +0200

    Primera clase de fundamentos

--- Fundamentos JAVA/src/App.java
+++ Fundamentos JAVA/src/App.java
@@ -1,5 +1,50 @@
 public class App {
+
+    public static boolean logico= true;
+    final static double PI= 3.141697;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*Estructura de la variable
+         * (TIPO) IDENTIFICADOR = VALOR
+        */
+
+        String cadena = "Me llamo Claudia";
+        int edad = 40;
+        double salario= 14.56789635268;
+        int x=5;   int y=7;     int z=10;
+        int xx=5,  yy=7 , zz=10;
+
+        int variable= 0;
+        System.out.println(variable);
+        variable= 33;
+        System.out.println(variable);
+
+        System.out.println(cadena);
+
+        int valor = 1/3;
+        double valor2 = 1/3.0;
+        System.out.println(valor);
+        System.out.println(valor2);
+
+        double a = (int)2.6;
+        System.out.println(a);
+        boolean logico2 = false;
+
+        
+        System.out.println(logico);
+        System.out.println(PI);
+        
+        int b = 'b';// un caracter se guarda en un entero según ASCII
+        System.out.println(b);
+        char c = 97; //Un entero se guarda en un caracter según ASCII
+        System.out.println(c);
+    }
+
+    /**
+     * Programa para imprimir texto
+     * @author Claudia Raquel Rubio Lazo
+     * @Claurubio7
+     */
+    public static void imprimirTexto(){
+            int logico=3;
     }
 }
