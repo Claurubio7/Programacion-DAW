5ce6c21c53a4c01cbafbb9725581d5e754a24c93
Claurubio7 <claulazorubio27@gmail.com>
2025-09-29 21:22:20 +0200

    Scanner y Funciones

--- Fundamentos JAVA/src/EntradaSalida.java
+++ Fundamentos JAVA/src/EntradaSalida.java
@@ -1,36 +1,66 @@
+import java.util.Scanner;
+
 public class EntradaSalida {
     public static void main(String[] args) {
-        //Instrucciones de salida por pantalla
-        int valor = 7;
-        System.out.println(valor);
 
-        //Usar printf (formato)
-        double precio= 13.354789;
-        System.out.printf("El precio del producto %s tiene un valor de %5.2f y se han comprado %d", "Tablet", precio, valor);
+        // //Instrucciones de salida por pantalla
+        // int valor = 7;
+        // System.out.println(valor);
 
-        //Incluir un icono
-        System.out.println("Que ganas tengo de unas vacaciones \u26F1");
+        // //Usar printf (formato)
+        // double precio= 13.354789;
+        // System.out.printf("El precio del producto %s tiene un valor de %5.2f y se han comprado %d", "Tablet", precio, valor);
 
-        //Formato reciente de System
-        System.out.println("El precio del producto " + "Tablet " + " tiene un valor de " + precio + " y se han comprado " + valor);
+        // //Incluir un icono
+        // System.out.println("Que ganas tengo de unas vacaciones \u26F1");
 
-        //Formato salida texto
-        System.out.printf("%15s, %-15s\n", "Domingo", "Informático");
+        // //Formato reciente de System
+        // System.out.println("El precio del producto " + "Tablet " + " tiene un valor de " + precio + " y se han comprado " + valor);
 
-        //Formato tabla 
-        System.out.println("\n|----------|----------|----------|");
-        System.out.printf("|%-10s|%-10s|%-10s|", "Producto", "Cantidad", "Valor" );
-        System.out.println("\n|----------|----------|----------|");
-        System.out.printf("|%-10s|%d         |%f |", "Producto", valor, precio );
-        System.out.println("\n|----------|----------|----------|");
+        // //Formato salida texto
+        // System.out.printf("%15s, %-15s\n", "Domingo", "Informático");
 
-        System.out.println("\n|----------|----------|----------|");
-        System.out.print("|"+ "Producto  " + "|" + "Cantidad  " + "|" + "Valor     " + "|" );
-        System.out.println("\n|----------|----------|----------|");
-        System.out.print("|"+ "Tablet    " + "|" + valor + "         |" + precio + " |" );
-        System.out.println("\n|----------|----------|----------|");
+        // //Formato tabla 
+        // System.out.println("\n|----------|----------|----------|");
+        // System.out.printf("|%-10s|%-10s|%-10s|", "Producto", "Cantidad", "Valor" );
+        // System.out.println("\n|----------|----------|----------|");
+        // System.out.printf("|%-10s|%d         |%f |", "Producto", valor, precio );
+        // System.out.println("\n|----------|----------|----------|");
+
+        // System.out.println("\n|----------|----------|----------|");
+        // System.out.print("|"+ "Producto  " + "|" + "Cantidad  " + "|" + "Valor     " + "|" );
+        // System.out.println("\n|----------|----------|----------|");
+        // System.out.print("|"+ "Tablet    " + "|" + valor + "         |" + precio + " |" );
+        // System.out.println("\n|----------|----------|----------|");
+
+        //Operaciones de entrada por teclado 
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Introduce la cadena de texto deseada");
+        // String cadena = sc.nextLine();
+        // System.out.println("El cadena intoducido es: " +  cadena);
+
+        System.out.print("Introduce Nombre: ");
+        //Lee nombre
+        String nombre = sc.nextLine();
+        //Leer Apellido
+        System.out.print("Introduce Apellido: ");
+        String apellido = sc.nextLine(); //Si se pidiera de uno en uno hay que poner nextLine()
+        //Leer edad
+        System.out.print("Introduce Edad: ");
+        // //Opción 1 para limpiar el buffer
+        // int edad = sc.nextInt();
+        // sc.nextLine(); //limpiar el buffer para el salto de linea
+        //Opción 2 para limpiar el buffer
+        int edad = Integer.parseInt(sc.nextLine());
+        //Leer profesión
+        System.out.print("Introduce Profesión: ");
+        String profesion = sc.nextLine();
+
+        System.out.println("El ususario se llama " + nombre + " " + apellido + ". Tiene " + edad + " años y trabaja de " + profesion );
 
 
+
+        sc.close(); //Cierre de el flujo de teclado. Si no lo haces tú ya lo hará el recolector de basura de JAVA 
 
 
     }
---
+++ Fundamentos JAVA/src/Funciones.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class Funciones {
+
+    public static void main(String[] args) {
+        //Tratamiento de fechas en Java
+
+        LocalDateTime hoy = LocalDateTime.now();
+        System.out.println(hoy.toString());
+        System.out.println(hoy.getDayOfWeek());
+    }
+    
+}
