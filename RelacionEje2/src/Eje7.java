f05e7ca1785648b685d34fd3a1e3d7c195799b1d
Claurubio7 <claulazorubio27@gmail.com>
2025-10-21 15:24:24 +0200

    EJERCICIOS

--- RelacionEje2/src/Eje7.java
+++ RelacionEje2/src/Eje7.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Eje7 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        int positivos = 0; 
+        int negativos = 0; 
+
+        System.out.println("Introduce números (0 para terminar):");
+
+        // Bucle que se repite hasta que el usuario escriba 0
+        do {
+            System.out.print("Número: ");
+            numero = sc.nextInt();
+
+            if (numero > 0) {
+                positivos++; 
+            } else if (numero < 0) {
+                negativos++; 
+            }
+
+        } while (numero != 0);
+
+        System.out.println("\n--------------------------------");
+        System.out.println("Cantidad de números positivos: " + positivos);
+        System.out.println("Cantidad de números negativos: " + negativos);
+        System.out.println("--------------------------------");
+        
+    }
 }
+
